b3175fe38caa41d8a82667992622da836fc6c275
Grzegorz R <ryniewiczg@gmail.com>
2025-12-21 18:13:16 +0100

    Update Section 1: CONCAT & SUBSTRING.sql

--- SQL/Practical Examples_3/Section 1: CONCAT & SUBSTRING.sql
+++ SQL/Practical Examples_3/Section 1: CONCAT & SUBSTRING.sql
@@ -3,7 +3,7 @@
 SELECT 
   CONCAT(Firstname, ' ', LastName) AS Full_Name,
   *
-FROM `plasma-climber-480814-i2.Practima_Meeting_5.Customers`;
+FROM customers`;
 
 
 2. Add the prefix “Product:” before each product name.
@@ -11,3 +11,17 @@ FROM `plasma-climber-480814-i2.Practima_Meeting_5.Customers`;
 SELECT 
   CONCAT('Product: ', product_name) AS product_name
 FROM products;
+
+
+3. Extract the first 5 characters from product names and name the column “Short”.
+
+SELECT 
+  SUBSTRING(product_name, 1, 5) AS "Short"
+FROM products;
+
+
+4. Extract the last 3 characters from customers’ email addresses.
+
+SELECT 
+  SUBSTRING(email, LENGTH(email) - 2, 3) AS email_suffix
+FROM customers;
